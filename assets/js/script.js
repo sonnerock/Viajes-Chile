481f13f9461ea0596be10ae04d0bc79599cdc939
mario <mario_style_3088@hotmail.com>
2024-02-23 17:50:15 -0300

    modificacion 5 evento scroll

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Tooltip enabling
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//Apariencia navbar
+
+let barraNavegador = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 100) {
+        barraNavegador.classList.add('bg-dark', 'shador')
+    }
+    else {
+        barraNavegador.classList.remove('bg-dark', 'shador')
+    }
+})
